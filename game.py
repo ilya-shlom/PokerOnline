f90114e3016e90e4630c0ffdc7211deb8e1ce79a
dann1erel <dannierel0@gmail.com>
2022-10-27 17:29:17 +0300

    rename the file and add new file

--- game.py
+++ game.py
@@ -1,12 +1,22 @@
 from classes import *
 
-def check_
+
+# c_on_t - card on table, inp - player's input
+def check_cards(c_on_t, inp):
+    if inp[0] == c_on_t[0]:
+        if inp[1] > c_on_t[1]:
+            return True
+        else:
+            return False
+    elif inp[0] == tr:
+        return True
+    else:
+        return False
+
 
 
 
 players = [Player(), Player()]
-
-
 
 deck = CardDeck()
 deck.mix()
@@ -19,4 +29,3 @@ for p in players:
 
 print(players[0].hand_deck)
 print(players[1].hand_deck)
-
