f198d46f6c1113bfb916635abd545eb1217e13b6
ilya-shlom <ilyashlom32@gmail.com>
2022-10-27 16:59:05 +0300

    2 players

---
+++ game.py
@@ -0,0 +1,14 @@
+from classes import *
+
+players = [Player(), Player()]
+
+deck = CardDeck()
+deck.mix()
+
+for p in players:
+    for i in range(6):
+        p.get_card(deck.get())
+    p.hand_deck.sort()
+
+print(players[0].hand_deck)
+print(players[1].hand_deck)
