03cea3617fda5c33564c3a3ee63da019901b8572
Ilya Shlom <ilyashlom32@gmail.com>
2022-11-15 19:33:37 +0300

    мама я фронтендер!!!

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
+api = Api(app)
 
 
 @app.route('/')
@@ -11,6 +13,23 @@ def index():
 @app.route('/gamepage')
 def gamepage():
     return render_template("gamepage.html")
+
+
+@app.route('/signup', methods=('GET', 'POST'))
+def signup():
+    return render_template("signup.html")
+
+
+@app.route('/submit_acc', methods=('GET', 'POST'))
+def submit_acc():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        print(12)
+        data = open("logins.txt", "w")
+        data.write(f'{username}\n{password}')
+    return "Вы успешно зарегистрировались!"
 
 
 if __name__ == "__main__":
