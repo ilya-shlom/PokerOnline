c452ac0eb20b0154873eddf7b54b1bb1225a9477
dann1erel <dannierel0@gmail.com>
2022-11-15 19:21:11 +0300

    start working with databases by Danil Kashuba the best man

---
+++ scripts.sql
@@ -0,0 +1,11 @@
+CREATE TABLE  users{
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    password VARCHAR(20) TEXT NOT NULL,
+};
+
+CREATE TABLE post{
+    player_id INTEGER PRIMARY KEY,
+    res VARCHAR(1),
+    using_cheats VARCHAR(1),
+};
