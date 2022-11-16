023df3df3fadef9058e8229550bd6a682f274f3e
Ilya Shlom <ilyashlom32@gmail.com>
2022-11-16 13:35:16 +0300

    db works

--- scripts.sql
+++ scripts.sql
@@ -1,16 +1,18 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS stats;
 
-CREATE TABLE  users{
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE  users
+(
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
     username VARCHAR(20) UNIQUE NOT NULL,
-    password VARCHAR(20) NOT NULL,
-};
+    password VARCHAR(20)        NOT NULL
+);
 
-CREATE TABLE stats{
-    player_id INTEGER PRIMARY KEY,
-    game_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    res VARCHAR(1),
+CREATE TABLE stats
+(
+    player_id    INTEGER,
+    game_id      INTEGER PRIMARY KEY AUTOINCREMENT,
+    res          VARCHAR(1),
     using_cheats VARCHAR(1),
     FOREIGN KEY (player_id) REFERENCES users (id)
-};
+);
