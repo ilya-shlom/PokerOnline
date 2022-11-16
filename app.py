c1b90167e24d7e5ea3cfca0f7cb7c42c42812d41
Ilya Shlom <ilyashlom32@gmail.com>
2022-11-16 21:58:56 +0300

    login system finalized (except password encryption, will definitely be added)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, flash, session, redirect, url_for
+from flask import Flask, render_template, request, flash, session, redirect, url_for, blueprints
 from flask_restful import Api, Resource, reqparse
 import db
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,7 +10,18 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    log_state = 0
+    database = db.get_db()
+    user_id = session.get('user_id', default=None)
+    if user_id:
+        log_state = 1
+        user = user_id
+        print(user)
+        user = database.execute('SELECT * FROM users WHERE id = ?', (user_id,)).fetchone()['username']
+
+    else:
+        user = "Not logged in"
+    return render_template("index.html", out=str(user), state=log_state)
 
 
 @app.route('/gamepage')
@@ -49,19 +61,24 @@ def submit_acc():
                 database.execute("INSERT INTO users (username, password) VALUES (?, ?)",
                                  (username, password))
                 database.commit()
+                session.clear()
+                session['user_id'] = database.execute('SELECT * FROM users WHERE username = ?',
+                                                      (username,)).fetchone()['id']
+                return render_template("index.html", out=f"Welcome to the game, {username}!")
             except database.IntegrityError:
                 error = f"User {username} is already registered."
             else:
                 return render_template("index.html")
         flash(error)
-    return render_template("signup.html")
+    return render_template("signup.html", error=error)
 
 
 @app.route('/signin', methods=('GET', 'POST'))
 def signin():
     return render_template("login.html")
 
-@app.route('/login', methods = ('GET', 'POST'))
+
+@app.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -72,19 +89,25 @@ def login():
 
         if user is None:
             error = 'Incorrect Username'
-        elif user['password'] is not password:
+        elif user['password'] != password:
             error = 'Incorrect Password'
 
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return render_template("index.html")
+            return render_template("index.html", out=f"Welcome back, {username}!")
 
         flash(error)
 
-    return render_template("index.html")
+    return render_template("login.html", error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
-    app.secret_key = "very secret key"
+    app.secret_key = os.urandom(30).hex()
     app.run(debug=True)
-
